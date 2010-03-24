a18833c152216e5efe8e91af0f9373068e12cb51
Laurence Rowe <l@lrowe.co.uk>
2010-03-24 01:55:38 +0000

    typo
    svn path=/diazo/trunk/; revision=44937

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ development, check it into Subversion, and not touch XDV during deployment.''',
     packages=find_packages('lib'),
     package_dir = {'':'lib'},
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
     author='Paul Everitt, Laurence Rowe and Martin Aspeli.',
     maintainer='Laurence Rowe',
     maintainer_email='laurence@lrowe.co.uk',
