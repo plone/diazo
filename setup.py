cdb100bd4b285f1ecba2fae199898fcce2e639b5
Laurence Rowe <l@lrowe.co.uk>
2010-05-23 21:15:22 +0000

    prepare for 0.3rc1 release
    svn path=/diazo/trunk/; revision=45409

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
+import os
+
 setup(
     name='xdv',
-    version='0.3a3',
+    version='0.3rc1',
     description='''\
 XDV implements a subset of Deliverance using a pure XSLT engine. With XDV, you
 "compile" your theme and ruleset in one step, then use a superfast/simple
 transform on each request thereafter. Alternatively, compile your theme during
 development, check it into Subversion, and not touch XDV during deployment.''',
-    long_description=open("README.txt").read(),
+    long_description=open("README.txt").read() + "\n\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
     packages=find_packages('lib'),
     package_dir = {'':'lib'},
     include_package_data=True,
