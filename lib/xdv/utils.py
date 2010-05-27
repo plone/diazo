ae37d474bf5c93d88dbbd727a9633a86afe485ad
Laurence Rowe <l@lrowe.co.uk>
2010-05-27 13:55:11 +0000

    unused code
    svn path=/diazo/trunk/; revision=45418

--- lib/xdv/utils.py
+++ lib/xdv/utils.py
@@ -1,5 +1,3 @@
-from lxml import etree
-
 namespaces = dict(
     xdv="http://namespaces.plone.org/xdv",
     css="http://namespaces.plone.org/xdv+css",
@@ -11,20 +9,3 @@ def localname(name):
 
 def fullname(namespace, name):
     return '{%s}%s' % (namespace, name)
-
-class Resolver(etree.Resolver):
-
-    def __init__(self, strings={}, files={}, filenames=[]):
-        self.strings = {}
-        self.files = {}
-        self.filenames = set(filenames)
-
-    def resolve(self, url, pubid, context):
-        result = self.strings.get(url)
-        if result is not None:
-            return self.resolve_string(result, context)
-        result = self.files.get(url)
-        if result is not None:
-            return self.resolve_file(result, context)
-        if url in self.filenames:
-            return self.resolve_filename(url, context)
