9941efad858b57bc57174c6e6085d2bf343dd1f3
Laurence Rowe <l@lrowe.co.uk>
2010-05-29 08:52:10 +0000

    0.3 release
    svn path=/diazo/trunk/; revision=44989

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='xdv',
-    version='0.3rc3',
+    version='0.3',
     description='''\
 XDV implements a subset of Deliverance using a pure XSLT engine. With XDV, you
 "compile" your theme and ruleset in one step, then use a superfast/simple
