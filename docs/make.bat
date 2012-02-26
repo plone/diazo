91482a311b7ae8d0ce0466dc7b88e8e494cb3907
Laurence Rowe <laurence@lrowe.co.uk>
2012-02-26 02:17:57 +0100

    Add sphinx to buildout

--- docs/make.bat
+++ docs/make.bat
@@ -3,7 +3,7 @@
 REM Command file for Sphinx documentation
 
 if "%SPHINXBUILD%" == "" (
-	set SPHINXBUILD=sphinx-build
+	set SPHINXBUILD=..\bin\sphinx-build
 )
 set BUILDDIR=_build
 set ALLSPHINXOPTS=-d %BUILDDIR%/doctrees %SPHINXOPTS% .
