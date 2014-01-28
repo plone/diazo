212a844101e87df1cf19f50ec091c9dfa3e9286c
Eric Steele <ericsteele47@gmail.com>
2014-01-27 21:37:02 -0500

    Fix changelog, version.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os.path
 
 setup(
     name='diazo',
-    version=''.dev0',
+    version='1.0.6.dev0',
     description='''Diazo implements a Deliverance like language using a pure
         XSLT engine. With Diazo, you "compile" your theme and ruleset in one
         step, then use a superfast/simple transform on each request thereafter.
