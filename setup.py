8fc043adc675e51edccde87e77ef0591e3c7a04a
Laurence Rowe <laurence@lrowe.co.uk>
2014-09-27 17:21:50 -0700

    Bump to 1.1.0 for Python3 compatibility.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ long_desc = readme + '\n\n' + changes
 
 setup(
     name='diazo',
-    version='1.0.7.dev0',
+    version='1.1.0.dev0',
     description='''Diazo implements a Deliverance like language using a pure
         XSLT engine. With Diazo, you "compile" your theme and ruleset in one
         step, then use a superfast/simple transform on each request thereafter.
