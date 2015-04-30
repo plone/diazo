55c01aa12e672b2e59f999b9d3b132355aa2829d
Huub Bouma <bouma@w20e.com>
2015-04-30 15:08:13 +0200

    classifiers gone, since upload to pypy resulted in error

--- setup.py
+++ setup.py
@@ -37,12 +37,6 @@ setup(
     license='New BSD',
     classifiers=[
         "Development Status :: 6 - Mature",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
         "Topic :: Internet :: WWW/HTTP",
         "Topic :: Internet :: WWW/HTTP :: WSGI",
         "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
