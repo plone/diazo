6948bbf2de786beee8948bcd8c6fc2f616c3ccd9
esteele <eric@esteele.net>
2017-02-12 16:23:50 -0500

    Preparing release 1.2.5
    [ci skip]

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ long_desc = readme + '\n\n' + changes
 
 setup(
     name='diazo',
-    version='1.2.5.dev0',
+    version='1.2.5',
     description='''Diazo implements a Deliverance like language using a pure
         XSLT engine. With Diazo, you "compile" your theme and ruleset in one
         step, then use a superfast/simple transform on each request thereafter.
